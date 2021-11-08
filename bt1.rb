d05708be036c6ee08e1b318c8c7eb6bd4c2c2649
thongnt0306@gmail.com <thongnt0306@gmail.com>
2021-11-08 16:21:04 +0700

    baitap1

---
+++ bt1.rb
@@ -0,0 +1,39 @@
+i = 0
+a = []
+
+puts "Nhap -1 de dung"
+loop {
+	#try-catch
+	begin
+		print "Nhap a[#{i}]:"
+		temp = Integer(gets.chomp)
+		break if temp == -1
+		raise if temp < 0
+		a[i] = temp
+	rescue
+		puts "Vui long nhap so tu nhien"
+		retry
+	end	
+	i+=1
+}
+
+puts "#{a}"
+#Cach 1 - Sap xep mang tang dan va lay vi tri 0
+# if a.length >= 1
+# 	a.sort!
+# 	min = a[0]
+# 	puts "Gia tri nho nhat la: #{min}"
+# elsif
+# 	puts "Khong co gia tri nho nhat"
+# end
+	
+#Cach 2 - Khong lam thay doi mang a
+if a.length >= 1
+	min = a[0]
+	for pos in 1..i-1
+		min = a[pos] if min > a[pos]
+	end
+	puts "Gia tri nho nhat la: #{min}"
+elsif
+	puts "Khong co gia tri nho nhat"
+end
