50045f64b455fd8510f1e228d42de480784c1349
thong <thongnt0306@gmail.com>
2021-11-10 00:29:09 +0700

    version 0.1

--- bt2.rb
+++ bt2.rb
@@ -1,1 +1,17 @@
-puts "Hello World"
+nhanvien = Hash.new
+puts "Nhap so thu tu"
+nhanvien[:stt] = gets.chomp
+puts "Nhap id"
+nhanvien[:id] = gets.chomp
+puts "Nhap ten"
+nhanvien[:ten] = gets.chomp
+puts "Nhap nam sinh"
+nhanvien[:namsinh] = gets.chomp
+puts "Nhap chuc vu"
+nhanvien[:chucvu] = gets.chomp
+puts "Nhap luong 1 ngay"
+nhanvien[:luongCoBan] = gets.chomp
+puts "Nhap ngay lam"
+nhanvien[:ngayLam] = gets.chomp
+
+nhanvien.each{|key, value| puts "#{key} #{value}"}
